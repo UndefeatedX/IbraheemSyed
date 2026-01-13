36a103f7c2af44e8395f5ea801efd4ed464ff5fd
UndefeatedX <ibraheemtsyed@gmail.com>
2026-01-12 19:19:27 -0500

    Update and rename main.js to script.js

--- main.js
+++
@@ -1,7 +0,0 @@
-// Optional: Background color button
-const btn = document.getElementById("btn");
-if (btn) {
-  btn.addEventListener("click", () => {
-    document.body.style.backgroundColor = "lightblue";
-  });
-}
