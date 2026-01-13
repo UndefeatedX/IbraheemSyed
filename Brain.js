96425852ff62435cf02ac127c16fb2f4920dccd2
UndefeatedX <ibraheemtsyed@gmail.com>
2026-01-12 19:04:22 -0500

    Add files via upload

---
+++ Brain.js
@@ -0,0 +1,7 @@
+// Optional: Background color button
+const btn = document.getElementById("btn");
+if (btn) {
+  btn.addEventListener("click", () => {
+    document.body.style.backgroundColor = "lightblue";
+  });
+}
